004505df1aa8ccdb01567aebd300ee88a05b1e1f
eyakhediri1-cyber <eyakhediri1@gmail.com>
2026-03-31 15:43:22 +0100

    Modification route accueil

--- migrations/Version20260331143431.php
+++ migrations/Version20260331143431.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260330112206 extends AbstractMigration
+final class Version20260331143431 extends AbstractMigration
 {
     public function getDescription(): string
     {
